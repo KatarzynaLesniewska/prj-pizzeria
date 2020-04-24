574b0c919f5983628add215e720d7b72720705df
Kasia Leśniewska <klesniewska29@gmail.com>
2020-04-24 16:51:13 +0200

    find menuContainer = document.querySelector()

--- src/js/script.js
+++ src/js/script.js
@@ -74,6 +74,7 @@
       thisProduct.element = utils.createDOMFromHTML(generatedHTML);
 
       /* find menu container */
+      const menuContainer = document.querySelector(select.containerOf.menu);
 
       /* add elem to menu */
 
