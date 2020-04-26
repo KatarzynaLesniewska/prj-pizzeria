be8487ce6a885580ab83d9ae57fc190c4989f794
Kasia Leśniewska <klesniewska29@gmail.com>
2020-04-26 19:23:18 +0200

    AddEventListener to from/input/cart & call processOrder

--- src/js/script.js
+++ src/js/script.js
@@ -123,6 +123,22 @@
     initOrderForm(){
       const thisProduct = this;
       console.log('initOrderForm- działa');
+
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
     }
 
     processOrder() {
