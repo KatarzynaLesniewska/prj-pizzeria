f242f462719a4f00555de7459b6222226bfc42b5
Kasia Leśniewska <klesniewska29@gmail.com>
2020-04-28 18:38:12 +0200

    Add getElements(element) to AmountWidget & call

--- src/js/script.js
+++ src/js/script.js
@@ -189,8 +189,19 @@
     constructor(element) {
       const thisWidget = this;
 
+      thisWidget.getElements(element);
+
       // console.log('AmountWidget:', thisWidget);
       // console.log('constructor arguments:', element);
+
+      getElements(element){
+        const thisWidget = this;
+
+        thisWidget.element = element;
+        thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+        thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+        thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+      }
     }
   }
 
