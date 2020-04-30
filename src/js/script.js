03f3ae5b8933594d73034c29f795b569ea348f08
Kasia Leśniewska <klesniewska29@gmail.com>
2020-04-30 14:39:50 +0200

    Correct constructor in CartProduct

--- src/js/script.js
+++ src/js/script.js
@@ -372,17 +372,17 @@
     constructor(menuProduct, element) {
       const thisCartProduct = this;
 
-      thisCartProduct.id = menuProduct(id);
-      thisCartProduct.name = menuProduct(name);
-      thisCartProduct.price = menuProduct(price);
-      thisCartProduct.priceSingle = menuProduct(priceSingle);
-      thisCartProduct.amount = menuProduct(amount);
-
+      thisCartProduct.id = menuProduct.id;
+      thisCartProduct.name = menuProduct.name;
+      thisCartProduct.price = menuProduct.price;
+      thisCartProduct.priceSingle = menuProduct.priceSingle;
+      thisCartProduct.amount = menuProduct.amount;
       thisCartProduct.params = JSON.parse(JSON.stringify(menuProduct.params));
 
       thisCartProduct.getElements(element);
 
       console.log('thisCartProduct:', thisCartProduct);
+      console.log('productData:', menuProduct);
     }
 
     getElements(element) {
@@ -391,7 +391,6 @@
       thisCartProduct.dom = {};
 
       thisCartProduct.dom.wrapper = element;
-
       thisCartProduct.dom.amountWidget = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.amountWidget);
       thisCartProduct.dom.price = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.price);
       thisCartProduct.dom.edit = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.edit);
