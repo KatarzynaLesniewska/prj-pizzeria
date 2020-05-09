bdfd192077b3f4418be1ba3b94d9f31d6176cec4
Kasia Leśniewska <klesniewska29@gmail.com>
2020-05-09 10:41:18 +0200

    Import select & AmountWidget to cartProduct.js

--- src/js/components/cartProduct.js
+++ src/js/components/cartProduct.js
@@ -1,3 +1,6 @@
+import {select} from './settings.js';
+import AmountWidget from './components/amountWidget.js';
+
 class CartProduct {
   constructor(menuProduct, element) {
     const thisCartProduct = this;
