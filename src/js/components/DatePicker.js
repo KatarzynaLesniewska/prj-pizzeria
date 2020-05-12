3c4affdf64eb045e85a921ebaf431ba11b2af936
Kasia Leśniewska <klesniewska29@gmail.com>
2020-05-12 23:18:09 +0200

    Declare class DatePicker

---
+++ src/js/components/DatePicker.js
@@ -0,0 +1,11 @@
+import BaseWidget from './BaseWidget.js';
+
+class DatePicker extends BaseWidget {
+  constructor() {
+    super();
+
+    const thisWidget = this;
+  }
+}
+
+export default DatePicker;
