926c542ccfa28f86b1d99e330e4b62f7ce56fa35
Kasia Leśniewska <klesniewska29@gmail.com>
2020-05-19 12:37:12 +0200

    Debug HourPicker.js

--- src/js/components/HourPicker.js
+++ src/js/components/HourPicker.js
@@ -1,5 +1,6 @@
 import BaseWidget from './BaseWidget.js';
 import {settings, select} from '../settings.js';
+import {utils} from '../utils.js';
 
 class HourPicker extends BaseWidget {
   constructor(wrapper) {
@@ -21,31 +22,19 @@ class HourPicker extends BaseWidget {
 
     rangeSlider.create(thisWidget.dom.input);
 
-    // const input =
     thisWidget.dom.input.addEventListener('change', function () {
       thisWidget.value = thisWidget.dom.input;
     });
   }
 
-  // Metoda parseValue ma przekazywać otrzymaną wartość do funkcji utils.numberToHour
-  // i zwracać wartość otrzymaną z tej funkcji. Ta funkcja zamienia liczby na zapis
-  // godzinowy, czyli np. 12 na '12:00', a 12.5 na '12:30'
-  parseValue() {
-    // const thisWidget = this;
-
-    // return utiles.numberToHour(thisWidget.value);
-    return true;
+  parseValue(value) {
+    return utils.numberToHour(value);
   }
 
-  // może zwracac true... ?
   isValid() {
-    // const thisWidget = this;
-
     return true;
   }
 
-  // Metoda renderValue ma zamieniać
-  // zawartość elementu thisWidget.dom.output na wartość widgetu.
   renderValue() {
     const thisWidget = this;
 
